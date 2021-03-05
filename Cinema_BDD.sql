b8fb1cdcba05e1a03e61201cd059e6bcb33b3c76
Fagus <paul.faguet@free.fr>
2021-03-05 11:14:25 +0100

    sql, mcd and diagram

---
+++ Cinema_BDD.sql
@@ -0,0 +1,36 @@
+create database Cinema_DB;
+
+create table Clients (
+    Id char(36) primary key not null,
+    Name varchar(50) not null,
+    Firstname varchar(50) not null,
+    Phone varchar(20),
+    Email varchar(254) not null,
+    Password varchar(30) not null,
+    DateBirth date not null,
+    Handicapped bool
+);
+
+create table Films (
+    Id int primary key auto_increment,
+    Title varchar(100) not null,
+    FilmShow datetime not null,
+    RoomId int not null,
+    AgesRestriction bool not null
+);
+
+create table Rooms (
+  Id int primary key auto_increment,
+  Capacity int not null,
+  HandicappedFriendly bool not null
+);
+
+create table ReservationTickets (
+    ClientId char(36) not null,
+    FilmId int not null,
+    primary key (ClientId, FilmId),
+    foreign key (ClientId) references Clients(Id),
+    foreign key (FilmId) references Films(Id),
+    SeatNumber varchar(4)
+)
+
